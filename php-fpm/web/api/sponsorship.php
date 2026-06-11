10e1f6a7df22d1b01b065f76bdb73a58ee74643d
Reksaku <m.wyrzykowski.2000@gmail.com>
2026-06-11 08:48:22 +0200

    feat: acces to sponsorship link

---
+++ php-fpm/web/api/sponsorship.php
@@ -0,0 +1,118 @@
+<?php
+
+// Połączenie z bazą
+$host = getenv('MYSQL_HOST');
+$db   = getenv('MYSQL_DATABASE');
+$user = getenv('MYSQL_USER');
+$pass = getenv('MYSQL_PASSWORD');
+
+$dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+
+header('Content-Type: application/json; charset=utf-8');
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+} catch (PDOException $e) {
+    http_response_code(500);
+    echo 'DB error';
+    exit;
+}
+
+$stmt = $pdo->query("SELECT * FROM sponsorship
+                    WHERE service = 'Buy Me a Coffee'
+                    ORDER BY publication_date DESC
+                    LIMIT 1");
+$sponsorship_link = $stmt->fetchAll(PDO::FETCH_ASSOC);
+foreach ($sponsorship_link as &$r) {
+    if (!empty($r['publication_date'])) {
+        $r['publication_date'] = date('Y-m-d\TH:i:s', strtotime($r['publication_date']));
+    }
+}
+
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+
+$ip = $_SERVER['HTTP_X_REAL_IP'] ?? null;
+
+function anonymize_ip($ip) {
+    if (!$ip) return null;
+
+    if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
+        $parts = explode('.', $ip);
+        if (count($parts) === 4) {
+            $parts[3] = 'x';
+            return implode('.', $parts);
+        }
+    }
+
+    if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
+        $parts = explode(':', $ip);
+        $parts[count($parts)-1] = '0000';
+        return implode(':', $parts);
+    }
+
+    return null;
+}
+
+$anonIp = anonymize_ip($ip);
+
+$userAgent = $_SERVER['HTTP_USER_AGENT'] ?? '';
+
+$clientType = null;
+if (stripos($userAgent, 'firefox') !== false || stripos($userAgent, 'chrome') !== false || stripos($userAgent, 'safari') !== false || stripos($userAgent, 'macintosh') !== false) $clientType = 'browser';
+elseif (stripos($userAgent, 'application') !== false) $clientType = 'application';
+else $clientType = 'other';
+
+$clientVersion = null;
+if (stripos($userAgent, 'ClickyKeys') !== false) {
+    if (preg_match('/ClickyKeysApp\/([0-9\.]+)/', $userAgent, $matches)) {
+        $clientVersion = $matches[1];
+    }
+}
+
+$clientDistribution = null;
+if (stripos($userAgent, 'Distro') !== false) {
+    if (preg_match('/Distro\/([A-Za-z]+)/', $userAgent, $matches)) {
+        if ($matches[1] == 'github' || $matches[1] == 'store' || $matches[1] == 'dev'){
+            $clientDistribution = $matches[1];
+        }
+    }
+}
+
+$clientTrigger = 'sponsorship';
+
+$sql = "
+    INSERT INTO api_requests (
+        requested_at,
+        path,
+        anon_ip,
+        client_type,
+	    version,
+        distribution,
+        trigger_type
+    )
+    VALUES (
+        NOW(),
+        :path,
+        :anon_ip,
+        :client_type,
+	    :version,
+        :distribution,
+        :trigger_type
+    )
+";
+
+$stmt = $pdo->prepare($sql);
+$stmt->execute([
+    ':path'            => $path,
+    ':anon_ip'         => $anonIp,
+    ':client_type'     => $clientType,
+    ':version'	       => $clientVersion,
+    ':distribution'    => $clientDistribution,
+    ':trigger_type'    => $clientTrigger
+]);
+
+
+echo json_encode($sponsorship_link);
